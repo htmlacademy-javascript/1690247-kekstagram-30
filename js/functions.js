f9d00e28ad0ee3774e19c9f504672134ddf2ee48
KazuulHome <atomnerobat@gmail.com>
2023-10-07 17:44:44 +0300

    qwe

--- js/functions.js
+++ js/functions.js
@@ -12,9 +12,9 @@ function ternaryCheckLength(inputString, length) {
 function checkPalindrome(inputString) {
   let index = 0;
   let str = inputString.toLowerCase();
-  str = str.replaceAll(' ', '')
+  str = str.replaceAll(' ', '');
   for (let i = str.length - 1; i > 0; i--) {
-    if (str.charAt(index) != str.charAt(i)) {
+    if (str.charAt(index) !== str.charAt(i)) {
       return false;
     }
     index++;
@@ -23,12 +23,12 @@ function checkPalindrome(inputString) {
 }
 
 function numberExtraction(inputString) {
-  let str = '' + inputString;
+  const str = '' + inputString;
   let finalStr = '';
   for (let i = 0; i < str.length - 1; i++) {
     if (!(isNaN(str.charAt(i)))) {
-      finalStr += parseInt(str.charAt(i));
+      finalStr += parseInt(str.charAt(i), 10);
     }
   }
-  return parseInt(finalStr);
+  return parseInt(finalStr, 10);
 }
