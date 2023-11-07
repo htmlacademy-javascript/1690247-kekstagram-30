9d561151750f542a099f72533acdb72dc80acd8d
KazuulWork <atomnerobat@gmail.com>
2023-11-07 17:15:41 +0300

    finaly fixed events listeners

--- js/full-size.js
+++ js/full-size.js
@@ -11,14 +11,35 @@ const body = document.querySelector('body');
 const closeButton = bigPicture.querySelector('.big-picture__cancel');
 const initComments = 5;
 const commentsLoadButton = bigPicture.querySelector('.social__comments-loader');
+let currentPhoto;
+
+function handleEscapeKey(event) {
+  if (event.key === 'Escape') {
+    closeFullsize();
+  }
+}
 
 function closeFullsize() {
   bigPicture.classList.add('hidden');
   commentCounter.classList.remove('hidden');
   commentsLoadButton.classList.remove('hidden');
   body.classList.remove('modal-open');
-  document.removeEventListener('keydown');
-  commentsLoadButton.removeEventListener('click');
+  document.removeEventListener('keydown', handleEscapeKey);
+  commentsLoadButton.removeEventListener('click', loadMoreComments);
+}
+
+function loadMoreComments() {
+  let shownCommentCounter = Number(commentShown.textContent);
+  const commentsCounterBefore = shownCommentCounter;
+  shownCommentCounter += initComments;
+  commentShown.textContent = Math.min(shownCommentCounter, Number(commentTotal.textContent));
+  for (let i = commentsCounterBefore; i < shownCommentCounter && i < Number(commentTotal.textContent); i++) {
+    renderComment(currentPhoto.comments[i]);
+  }
+  if (shownCommentCounter >= Number(commentTotal.textContent)) {
+    commentsLoadButton.classList.add('hidden');
+    commentsLoadButton.removeEventListener('click', loadMoreComments);
+  }
 }
 
 function renderComment(comment) {
@@ -43,52 +64,30 @@ function renderComment(comment) {
 }
 
 function openFullsize(photo) {
+  currentPhoto = photo;
   bigPicture.classList.remove('hidden');
   imgElement.src = photo.url;
   imgElement.alt = photo.description;
   likesCountElement.textContent = photo.likes;
   commentTotal.textContent = photo.comments.length;
-  commentShown.textContent = initComments;
+  commentShown.textContent = Math.min(initComments, photo.comments.length);
   socialCaption.textContent = photo.description;
   socialComments.innerHTML = '';
-  let shownCommentCounter = Number(commentShown.textContent);
-  if (photo.comments.length <= initComments) {
-    commentShown.textContent = commentTotal.textContent;
-    photo.comments.forEach((comment) => {
-      renderComment(comment);
-    });
-    commentsLoadButton.classList.add('hidden');
+
+  for (let i = 0; i < commentShown.textContent; i++) {
+    renderComment(photo.comments[i]);
+  }
+
+  if (photo.comments.length > initComments) {
+    commentsLoadButton.classList.remove('hidden');
+    commentsLoadButton.addEventListener('click', loadMoreComments);
   } else {
-    for (let i = 1; i < initComments + 1; i++) {
-      renderComment(photo.comments[i - 1]);
-      commentShown.textContent = initComments;
-    }
-    commentsLoadButton.addEventListener('click', () => {
-      const commentsCounterBefore = shownCommentCounter;
-      shownCommentCounter += initComments;
-      commentShown.textContent = shownCommentCounter;
-      if (shownCommentCounter < Number(commentTotal.textContent)) {
-        for (let i = commentsCounterBefore; i < shownCommentCounter; i++) {
-          renderComment(photo.comments[i]);
-        }
-      } else {
-        commentShown.textContent = commentTotal.textContent;
-        for (let i = commentsCounterBefore; i < shownCommentCounter; i++) {
-          renderComment(photo.comments[i]);
-        }
-        commentsLoadButton.removeEventListener('click');
-      }
-      commentsLoadButton.classList.add('hidden'); //не могу добиться, чтобы исчезла после прогрузки всех фото
-    });
+    commentsLoadButton.classList.add('hidden');
   }
 
   body.classList.add('modal-open');
-  closeButton.addEventListener('click', () => closeFullsize());
-  document.addEventListener('keydown', (event) => {
-    if (event.key === 'Escape') {
-      closeFullsize();
-    }
-  });
+  closeButton.addEventListener('click', closeFullsize);
+  document.addEventListener('keydown', handleEscapeKey);
 }
 
 export { openFullsize };
